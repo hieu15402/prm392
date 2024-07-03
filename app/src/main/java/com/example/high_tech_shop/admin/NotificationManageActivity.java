41cb8ee4a454d9174e5aad10779f107f77723b37
phamduchuy <huypd284@gmail.com>
2024-07-03 11:47:57 +0700

    layout admin

---
+++ app/src/main/java/com/example/high_tech_shop/admin/NotificationManageActivity.java
@@ -0,0 +1,18 @@
+package com.example.high_tech_shop.admin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.high_tech_shop.R;
+
+public class NotificationManageActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_notification_manage);
+
+            // Implement your notification management functionalities here
+        }
+}
---
+++ app/src/main/java/com/example/high_tech_shop/admin/PaymentManageActivity.java
@@ -0,0 +1,17 @@
+package com.example.high_tech_shop.admin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.high_tech_shop.R;
+
+public class PaymentManageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_manage);
+
+        // Implement your payment management functionalities here
+    }
+}
